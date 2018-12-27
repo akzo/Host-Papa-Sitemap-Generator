e80b9184bb30cdb052c7bdd32e233e2bc59a84b2
akzo <anarkazimov98@hotmail.com>
2018-12-26 19:20:43 -0800

    Adding ftp function

---
+++ siteMapUpdater.py
@@ -0,0 +1,26 @@
+# establishing ftp connection
+# link to the api
+from ftplib import FTP
+
+def upload_to_hostPapa(ftpHost, account, password, sitemapName):
+    
+    ftp = FTP(ftpHost, account, password)
+    ftp.cwd('public_html')
+    sitemap = open(sitemapName, 'rb')
+    ftpStoreCommand = 'STOR ' + sitemapName
+    ftp.storbinary(ftpStoreCommand, sitemap)
+    sitemap.close()
+    ftp.quit()
+
+    print('Sitemap was successfully uploaded')
+    
+
+
+
+
+ftpLink = input('Enter ftp link: ')
+ftpAccount = input('Enter ftp account: ')
+ftpPassword = input('Enter ftp password: ')
+upload_to_hostPapa(ftpLink, ftpAccount, ftpPassword, 'a.txt')
+
+
